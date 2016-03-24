ee3456c2f70ed857b0c56e132e2edb30d330ceb6
jserodio <serodio.jose@protonmail.ch>
2016-03-24 03:44:18 +0100

    initial commit

---
+++ dnd/filedrag.js
@@ -0,0 +1,74 @@
+// getElementById
+	function $id(id) {
+		return document.getElementById(id);
+	}
+
+
+	// output information
+	function Output(msg) {
+		var m = $id("messages");
+		m.innerHTML = m.innerHTML + msg;
+	}
+
+
+	// file drag hover
+	function FileDragHover(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.target.className = (e.type == "dragover" ? "hover" : "");
+	}
+
+
+	// file selection
+	function FileSelectHandler(e) {
+
+		// cancel event and hover styling
+		FileDragHover(e);
+
+		// fetch FileList object
+		var files = e.target.files || e.dataTransfer.files;
+
+		// process all File objects
+		for (var i = 0, f; f = files[i]; i++) {
+			ParseFile(f);
+		}
+
+	}
+
+
+	// output file information
+	function ParseFile(file) {
+
+		Output(
+			"<p>Datos del fichero: <strong>" + file.name +
+			"</strong> Tipo: <strong>" + file.type +
+			"</strong> Tamaño: <strong>" + file.size +
+			"</strong> bytes</p>"
+		);
+
+	}
+
+
+	// initialize
+	function Init() {
+
+            // file selected
+            var fileselect = $id("fileselect");
+            fileselect.addEventListener("change", FileSelectHandler, false);
+
+			var filedrag = $id("filedrag");
+			var submitbutton = $id("submitbutton");
+
+			// file drop
+			filedrag.addEventListener("dragover", FileDragHover, false);
+			filedrag.addEventListener("dragleave", FileDragHover, false);
+			filedrag.addEventListener("drop", FileSelectHandler, false);
+			filedrag.style.display = "block";
+
+	}
+
+	// call initialization file
+	if (window.File && window.FileList) {
+		Init();
+	}
+
